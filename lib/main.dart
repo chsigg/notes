44d1c6cc56ac3eb9c170aae63fdc37ceeed0946f
Christian Sigg <me@csigg.com>
2025-05-11 08:46:28 +0200

    Remove _MyApp, it's not needed.

--- lib/main.dart
+++ lib/main.dart
@@ -14,48 +14,36 @@ void main() async {
   fontLoader.addFont(rootBundle.load('assets/fonts/staff_clef_pitches.ttf'));
   await fontLoader.load();
 
+  const seedColor = Colors.pink;
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => SessionsProvider()),
         ChangeNotifierProvider(create: (context) => SettingsProvider()),
       ],
-      child: _MyApp(),
+      child: Consumer<SettingsProvider>(
+        builder:
+            (context, settings, child) => MaterialApp(
+              title: "Helma's Note Trainer",
+              theme: ThemeData(colorSchemeSeed: seedColor),
+              darkTheme: ThemeData(
+                colorSchemeSeed: seedColor,
+                brightness: Brightness.dark,
+              ),
+              supportedLocales: NoteLocalizations.supportedLanguages.map(
+                (language) => Locale(language),
+              ),
+              localizationsDelegates: [
+                NoteLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              locale:
+                  settings.language != null ? Locale(settings.language!) : null,
+              home: HomePage(),
+            ),
+      ),
     ),
   );
-}
-
-class _MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<_MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    const seedColor = Colors.pink;
-    return Consumer<SettingsProvider>(
-      builder:
-          (context, settings, child) => MaterialApp(
-            title: "Helma's Note Trainer",
-            theme: ThemeData(colorSchemeSeed: seedColor),
-            darkTheme: ThemeData(
-              colorSchemeSeed: seedColor,
-              brightness: Brightness.dark,
-            ),
-            supportedLocales: NoteLocalizations.supportedLanguages.map(
-              (language) => Locale(language),
-            ),
-            localizationsDelegates: [
-              NoteLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            locale:
-                settings.language != null ? Locale(settings.language!) : null,
-            home: HomePage(),
-          ),
-    );
-  }
 }
